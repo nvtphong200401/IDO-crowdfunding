fa8cfb41646a48f829dbfda2fcf35e048728607a
nvtphong <nvtphong20042001@gmmail.com>
2023-08-09 20:58:19 +0700

    create contract Project

---
+++ contracts/Project.sol
@@ -0,0 +1,49 @@
+contract Project {
+    // Project state
+    enum State {
+        Fundraising,
+        Expired,
+        Successful
+    },
+
+    
+    // Structs
+
+    struct WithdrawRequest{
+        string description;
+        uint256 amount;
+        uint256 noOfVotes;
+        mapping(address => bool) voters;
+        bool isCompleted;
+        address payable reciptent;
+    }
+
+    // Variables
+    address payable public creator;
+    uint256 public minimumContribution;
+    uint256 public deadline;
+    uint256 public targetContribution; // required to reach at least this much amount
+    uint public completeAt;
+    uint256 public raisedAmount; // Total raised amount till now
+    uint256 public noOfContributers;
+    string public projectTitle;
+    string public projectDes;
+    State public state = State.Fundraising; 
+
+    mapping (address => uint) public contributiors;
+    mapping (uint256 => WithdrawRequest) public withdrawRequests;
+
+    uint256 public numOfWithdrawRequests = 0;
+
+    // Modifiers
+    modifier isCreator(){
+        require(msg.sender == creator,'You dont have access to perform this operation !');
+        _;
+    }
+
+    modifier validateExpiry(State _state){
+        require(state == _state,'Invalid state');
+        require(block.timestamp < deadline,'Deadline has passed !');
+        _;
+    }
+}
