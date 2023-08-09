e2101adcff3c912a921687d10df4efac9c2cbec2
nvtphong <nvtphong20042001@gmmail.com>
2023-08-09 20:27:39 +0700

    add dependencies for hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
-require("@nomicfoundation/hardhat-toolbox");
+// require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  paths: {
+    artifacts: "./client/artifacts",
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337
+    },
+    // rinkeby: {
+    //   url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+    //   accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+    // }
+  },
 };
